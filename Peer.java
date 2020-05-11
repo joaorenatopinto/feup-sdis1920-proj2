0652c3145dda625586bc1efd863b8e7c4d90e150
João Renato Pinto <jrcp210@gmail.com>
2020-05-11 16:41:08 +0100

    Added thread to permantly listen

--- Peer.java
+++ Peer.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
@@ -6,25 +5,25 @@ import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import javax.net.ssl.SSLServerSocket;
-import javax.net.ssl.SSLServerSocketFactory;
-import javax.net.ssl.SSLSocket;
 
 public class Peer {
     static public Node chordNode;
     static public boolean shutdown = false;
+    static public String ipAddress = null;
+    static public int portNumber;
+    static public ScheduledExecutorService pool;
 
-    public static void main(final String[] args) throws NoSuchAlgorithmException {
-        final Peer peer = new Peer();
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Peer peer = new Peer();
         peer.run(args);
     }
 
-    public void run(final String[] args) throws NoSuchAlgorithmException {
+    public void run(String[] args) throws NoSuchAlgorithmException {
         System.setProperty("javax.net.ssl.trustStore", "truststore");
         System.setProperty("javax.net.ssl.trustStorePassword", "123456");
 
-        final int portNumber = Integer.parseInt(args[1]);
-        final String ipAddress = args[2];
+        portNumber = Integer.parseInt(args[1]);
+        ipAddress = args[2];
         final String chordOption = args[3];
 
         chordNode = new Node(ipAddress, portNumber, this);
@@ -41,18 +40,7 @@ public class Peer {
             System.out.println("vai te foder burro do caralho");
             return;
         }
-        SSLServerSocket serverSocket = null;
-        SSLServerSocketFactory ssf = null;
-
-        ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-
-        try {
-            serverSocket = (SSLServerSocket) ssf.createServerSocket(portNumber);
-        } catch (final IOException e) {
-            System.out.println("Server - Failed to create SSLServerSocket");
-            e.getMessage();
-            return;
-        }
+        
         try {
             final PeerMethods peer = new PeerMethods();
 
@@ -66,35 +54,13 @@ public class Peer {
             return;
         }
         System.err.println("Peer ready");
-        final ScheduledExecutorService pool = Executors.newScheduledThreadPool(50);
+         pool = Executors.newScheduledThreadPool(50);
 
         // Create task where stabilizes and notifies chord and gives it to ThreadPool to execute it every second
         final Runnable chordhandle = new ChordHandler(chordNode);
         pool.scheduleAtFixedRate(chordhandle, 20, 20, TimeUnit.SECONDS);
-
-
-        try {
-            System.out.println("Waiting Connection...");
-            while (true) {
-               
-                //waits for a connection to occur and creates a Message Processor task and gives it to ThreadPool
-                final SSLSocket clientSocket = (SSLSocket) serverSocket.accept();
-                System.out.println("Recebi cenas");
-                final Runnable task = new MessageProcessor(clientSocket);
-                pool.execute(task);
-
-                if (shutdown) {
-                    System.out.println("Closing all sockets and threadPool...");
-                    pool.shutdown();
-                    clientSocket.close();
-                    serverSocket.close();
-                    System.out.println("Shuting down...");
-                    System.out.println("Bye.");
-                    break;
-                }
-            }
-        } catch (final IOException e) {
-                e.printStackTrace();
-            }
+        // Create task where a thread permantly is listening to the server socket and gives it to the ThreadPool
+        final Runnable listener = new PeerThread(this);
+        pool.execute(listener);
     }
 }   
---
+++ PeerThread.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+
+public class PeerThread implements Runnable {
+    public Peer peer;
+
+    public PeerThread (Peer peer) {
+        this.peer = peer;
+    }
+
+    @Override
+    public void run() {
+        SSLServerSocket serverSocket = null;
+        SSLServerSocketFactory ssf = null;
+
+        ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+
+        try {
+            serverSocket = (SSLServerSocket) ssf.createServerSocket(Peer.portNumber);
+        } catch (final IOException e) {
+            System.out.println("Server - Failed to create SSLServerSocket");
+            e.getMessage();
+            return;
+        }
+        try {
+            //System.out.println("Waiting Connection...");
+            while (true) {
+                // waits for a connection to occur and creates a Message Processor task and gives it to ThreadPool
+                final SSLSocket clientSocket = (SSLSocket) serverSocket.accept();
+                System.out.println("Recebi cenas");
+                final Runnable task = new MessageProcessor(clientSocket);
+                Peer.pool.execute(task); 
+            }
+        } catch (final IOException e) {
+                e.printStackTrace();
+            }
+    }
+}
